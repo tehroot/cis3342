7c066c0c558b9fa50f5bdfa4845875453e07a1c2
TehRoot <cardish@runbox.com>
2019-09-13 00:35:58 -0400

    project 1 commit up to date

---
+++ Project1/Project1/quiz_results.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Quiz_results.aspx.cs" Inherits="Project1.Quiz_results" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+             <span>The big gay</span>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Project1/Project1/quiz_results.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Project1 {
+    public partial class Quiz_results : System.Web.UI.Page {
+        protected void Page_Load(object sender, EventArgs e) {
+            foreach (String s in Request.Form.AllKeys) {
+                //lmao the collection sort shit L M A O
+
+                Debug.WriteLine(Request.Form[s]);
+            }
+        }
+    }
+}
