18b6391b06f7a915235a074e0d726e19c99f0147
TehRoot <cardish@runbox.com>
2019-09-15 23:32:52 -0400

    quiz processing done

--- Project1/Project1/classes/Quiz.cs
+++ Project1/Project1/classes/Quiz.cs
@@ -1,20 +1,60 @@
 ﻿using System;
-using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.IO;
+using System.Web;
 namespace Project1 {
     public class Quiz {
-
-        public int student_id { get; set; }
-        public String student_name { get; set; }
-        public ArrayList questionList { get; set; }
+        //quiz has one attribute, a questionlist assembled from a loaded file located in the base project directory.
+        public Dictionary<int, String> questionSet { get; set; }
 
         public Quiz() {
             
         }
 
-        public Quiz(String student_name, int student_id ) {
-            this.student_id = student_id;
-            this.student_name = student_name;
-            
+        public void buildQuizKey() {
+            questionSet = new Dictionary<int, string>();
+            String filePath = HttpContext.Current.Request.PhysicalApplicationPath + "/answers.txt";
+            List<String> fileRead = new List<String>(File.ReadAllLines(filePath));
+            foreach (String s in fileRead) {
+                String[] splitStrings = s.Split(new[] { ',' }, 2);
+                questionSet.Add(int.Parse(splitStrings[0]), splitStrings[1].TrimEnd('\\'));
+            }
         }
+
+        public IDictionary<int, Boolean> gradeQuiz(Quiz quiz, NameValueCollection nameValueCollection) {
+            IDictionary<String, String> dict = new Dictionary<String, String>();
+            IDictionary<int, String> new_dict = new Dictionary<int, String>();
+            IDictionary<int, Boolean> big_dict = new Dictionary<int, Boolean>();
+            foreach (var k in nameValueCollection.AllKeys) {
+                dict.Add(k, nameValueCollection[k]);
+            }
+            dict.Remove("firstName");
+            dict.Remove("tuid");
+            foreach (var k in dict.Keys) {
+                new_dict.Add(int.Parse(k), dict[k]);
+            }
+            foreach(int i in new_dict.Keys) {
+                if (questionSet[i] == new_dict[i]) {
+                    big_dict.Add(i, true);
+                } else {
+                    big_dict.Add(i, false);
+                }
+            }
+            return big_dict;
+        }
+
+        public Decimal scoreQuiz(IDictionary<int, Boolean> quizResults) {
+            decimal score = 0;
+            foreach (int i in quizResults.Keys) {
+                if (quizResults[i]) {
+                    score++;
+                }
+            }
+            return score / quizResults.Count;
+        }
+
+
     }
 }
--- Project1/Project1/quiz_results.aspx.cs
+++ Project1/Project1/quiz_results.aspx.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -8,11 +10,16 @@ using System.Web.UI.WebControls;
 
 namespace Project1 {
     public partial class Quiz_results : System.Web.UI.Page {
+
+        protected void Page_Init(object sender, EventArgs e) {
+            Quiz quiz = new Quiz();
+            quiz.buildQuizKey();
+            NameValueCollection request = Request.Form;
+            IDictionary<int, Boolean> score = quiz.gradeQuiz(quiz, request);
+
+        }
         protected void Page_Load(object sender, EventArgs e) {
-            foreach (String s in Request.Form.AllKeys) {
-                //lmao the collection sort shit L M A O
-                Debug.WriteLine(s + ", " +Request.Form[s]);
-            }
+            
         }
     }
 }
