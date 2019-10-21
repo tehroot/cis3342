cc2e66e736a5622b07bef822e5b87963c58adcd2
Mark Cardish <rewt@8c-85-90-84-ce-17.wireless.temple.edu>
2019-10-21 18:14:51 -0400

    class shit

--- Project3/Project3/Classes/Email.cs
+++ Project3/Project3/Classes/Email.cs
@@ -5,5 +5,70 @@ using System.Web;
 
 namespace Project3.Classes {
     public class Email {
+        private String _email_id { get; set; }
+        private String _email_sender { get; set; }
+        private String _email_recipient { get; set; }
+        private String _email_content { get; set; }
+        private String _email_subject { get; set; }
+        private Boolean _email_flag { get; set; }
+
+        public String email_id {
+            get { return _email_id; }
+            set {
+                if (value != null && value != "") {
+                    _email_id = value;
+                } else {
+                    //change exception type
+                    throw new ArgumentException("Email ID cannot be blank.");
+                }
+
+            }
+        }
+        public String email_sender {
+            get { return _email_sender; }
+            set {
+                if (value != null && value != "") {
+                    _email_sender = value;
+                } else {
+                    throw new ArgumentException("Email Sender cannot be blank.");
+                }
+            }
+        }
+        public String email_recipient {
+            get { return _email_recipient; }
+            set {
+                if (value != null && value != "") {
+                    _email_recipient = value;
+                } else {
+                    throw new ArgumentException("Email Recipient cannot be blank.");
+                }
+
+            }
+        }
+
+        public String email_content { get { return _email_content; }
+            set {
+                if (value != null) {
+                    _email_content = value;
+                } else {
+                    //change exception type, null not allowed
+                    throw new ArgumentException("Null content not allowed, error");
+                }
+            }
+        }
+        public String email_subject { get { return _email_subject; }
+            set {
+                if (value != null && value != "") {
+                    _email_subject = value;
+                } else {
+                    throw new ArgumentException("Email Subject cannot be blank.");
+                }
+            }
+        }
+        public Boolean email_flag { get { return _email_flag; }
+            set {
+                _email_flag = value;    
+            }
+        }
     }
 }
--- Project3/Project3/Classes/User.cs
+++ Project3/Project3/Classes/User.cs
@@ -5,6 +5,38 @@ using System.Web;
 
 namespace Project3.Classes {
     public class User {
+        private String _username { get; set; }
+        private String _password { get; set; }
+        private Boolean _adminFlag { get; set; }
+        private Boolean _banFlag { get; set; }
 
+        public String username { get { return _username; }
+            set {
+                if (value != null && value != "") {
+                    _username = value;
+                } else {
+                    throw new ArgumentException("Username cannot be blank.");
+                }
+            }
+        }
+        public String password { get { return _password; }
+            set {
+                if (value != null && value != "") {
+                    _password = value;
+                } else {
+                    throw new ArgumentException("Password cannot be blank.");
+                }
+            }
+        }
+        public Boolean adminFlag { get { return _adminFlag; }
+            set {
+                _adminFlag = value;
+            }
+        }
+        public Boolean banFlag { get { return _banFlag; }
+            set {
+                _banFlag = value;
+            }
+        }
     }
 }
